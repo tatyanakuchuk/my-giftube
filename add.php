85a96a863687f5308e421c30b730a7a4ce54618d
tatyanakuchuk <tatyana8787@gmail.com>
2019-12-17 22:49:45 +0100

    Добавление новой гифки

--- add.php
+++ add.php
@@ -20,11 +20,97 @@ if(!$connect) {
         $error = mysqli_error($connect);
         print('Ошибка MySQL: ' . $error);
     }
-}
 
-$add_form = include_template('add-form.php', [
-    'categories' => $categories,
-]);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $gif = $_POST;
+        $required = ['category', 'gif-title', 'gif-description'];
+        $errors = [];
+        $dict = [
+            'gif-img' => 'Гифка',
+            'category' => 'Категория',
+            'gif-title' => 'Название',
+            'gif-description' => 'Описание'
+        ];
+
+        foreach($required as $key) {
+            if (empty($_POST[$key])) {
+                $errors[$key] = 'Это поле должно быть заполнено';
+            }
+        }
+
+        if (isset($_FILES['gif-img']['name'])) {
+            if (empty($_FILES['gif-img']['name'])) {
+                $errors['gif-img'] = 'Вы не загрузили файл';
+            }
+            else {
+                $tmp_name = $_FILES['gif-img']['tmp_name'];
+                $file = $_FILES['gif-img']['name'];
+
+                $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                $file_type = finfo_file($finfo, $tmp_name);
+
+                // Получаем расширение загруженного файла
+                $extension = strtolower(substr(strrchr($file, '.'), 1));
+                //Генерируем новое имя файла
+                $file = uniqid() . '.' .  $extension;
+                //Папка назначения
+                $dest = 'uploads/';
+
+                if ($file_type !== "image/gif") {
+                    $errors['gif-img'] = 'Загрузите гифку в формате GIF';
+                }
+                else {
+                    move_uploaded_file($tmp_name, $dest . $file);
+                    $gif['img_path'] = $dest . $file;
+                }
+            }
+        }
+
+
+
+        if (count($errors)) {
+            $add_form = include_template('add-form.php', [
+                'gif' => $gif,
+                'categories' => $categories,
+                'errors' => $errors,
+                'dict' => $dict
+            ]);
+        }
+        else {
+
+
+            $sql = 'INSERT INTO gifs (dt_add, category_id, user_id, title, description, ' .
+                    'img_path, likes_count, favs_count, views_count) ' .
+                    'VALUES (NOW(), ?, ?, ?, ?, ?, ?, ?, ?)';
+
+            $stmt = db_get_prepare_stmt($connect, $sql, [
+                $gif['category'],
+                4,
+                $gif['gif-title'],
+                $gif['gif-description'],
+                $gif['img_path'],
+                6,
+                9,
+                15
+            ]);
+
+            $res = mysqli_stmt_execute($stmt);
+
+            if ($res) {
+                $gif_id = mysqli_insert_id($connect);
+                header('Location: /gif.php?id=' . $gif_id);
+            }
+        }
+
+    }
+    else {
+        $add_form = include_template('add-form.php', [
+            'gif' => $gif,
+            'categories' => $categories
+        ]);
+    }
+
+}
 
 $page_content = include_template('main.php', [
     'form' => $add_form,
--- templates/add-form.php
+++ templates/add-form.php
@@ -1,4 +1,4 @@
-<form class="form" action="" method="post" enctype="multipart/form-data">
+<form class="form" action="../add.php" method="post" enctype="multipart/form-data">
     <div class="form__columns">
         <div class="form__column form__column--short">
             <div class="form__row">
@@ -9,7 +9,8 @@
                         <img class="preview__img" src="img/no-pic.png" alt="" width="192" height="192">
                     </div>
                     <div class="form__input-file">
-                        <input class="visually-hidden" type="file" name="gif[path]" id="preview" value="">
+                        <?php $value = isset($gif['img_path']) ? $gif['img_path'] : ""; ?>
+                        <input class="visually-hidden" type="file" name="gif-img" id="preview" value="<?= $value; ?>">
                         <label for="preview" class="">
                             <span>Выбрать файл</span>
                         </label>
@@ -19,37 +20,63 @@
         </div>
 
         <div class="form__column">
+            <?php $classname = isset($errors['category']) ? "form__input--error" : ""; ?>
             <div class="form__row">
                 <label class="form__label" for="category">Категория:</label>
-                <select class="form__input form__input--select " name="gif[category]" id="category">
-                    <option value="">Выберите категорию</option>
 
+                <select class="form__input form__input--select <?= $classname; ?>" name="category" id="category">
+                    <option value="">Выберите категорию</option>
                     <?php foreach($categories as $category): ?>
-                        <option value="4"><?= $category['name']; ?></option>
+
+                        <option value="<?= $category['id']; ?>" <?php if (isset($gif['category']) && ($gif['category'] == $category['id'])) { print(' selected'); }; ?> ><?= $category['name']; ?></option>
                     <?php endforeach; ?>
 
                 </select>
+                <?php if(isset($errors['category'])) : ?>
+                    <div class="error-notice">
+                        <span class="error-notice__icon"></span>
+                        <span class="error-notice__tooltip">Это поле должно быть заполнено</span>
+                    </div>
+                <?php endif; ?>
             </div>
+            <?php $classname = isset($errors['gif-title']) ? "form__input--error" : "";
+            $value = isset($gif['gif-title']) ? $gif['gif-title'] : ""; ?>
             <div class="form__row">
                 <label class="form__label" for="name">Название:</label>
-                <input class="form__input " type="text" name="gif[title]" id="name" value="" placeholder="Введите название">
+                <input class="form__input <?= $classname; ?>" type="text" name="gif-title" id="name" value="<?= $value; ?>" placeholder="Введите название">
+                <?php if(isset($errors['gif-title'])) : ?>
+                    <div class="error-notice">
+                        <span class="error-notice__icon"></span>
+                        <span class="error-notice__tooltip">Это поле должно быть заполнено</span>
+                    </div>
+                <?php endif; ?>
             </div>
+            <?php $classname = isset($errors['gif-description']) ? "form__input--error" : "";
+            $value = isset($gif['gif-description']) ? $gif['gif-description'] : ""; ?>
             <div class="form__row">
                 <label class="form__label" for="description">Описание:</label>
-                <textarea class="form__input " name="gif[description]" id="description" rows="5" cols="80" placeholder="Краткое описание"></textarea>
+                <textarea class="form__input <?= $classname; ?>" name="gif-description" id="description" rows="5" cols="80" placeholder="Краткое описание"><?= $value; ?></textarea>
+                <?php if(isset($errors['gif-description'])) : ?>
+                    <div class="error-notice">
+                        <span class="error-notice__icon"></span>
+                        <span class="error-notice__tooltip">Это поле должно быть заполнено</span>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
 
     <!-- Сообщение об ошибках -->
-    <div class="form__errors">
-        <p>Пожалуйста, исправьте следующие ошибки:</p>
-        <ul>
-            <li><strong>Категория:</strong> Это поле должно быть заполнено</li>
-            <li><strong>Название:</strong> Это поле должно быть заполнено</li>
-            <li><strong>Описание:</strong> Это поле должно быть заполнено</li>
-        </ul>
-    </div>
+    <?php if(isset($errors)) : ?>
+        <div class="form__errors">
+            <p>Пожалуйста, исправьте следующие ошибки:</p>
+            <ul>
+                <?php foreach($errors as $error => $val) : ?>
+                    <li><strong><?= $dict[$error]; ?>:</strong> <?= $val; ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endif; ?>
     <!-- end Сообщение об ошибках -->
 
     <div class="form__controls">

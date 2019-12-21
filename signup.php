45e6d0afec5aeb286063eac09ad7575da983ad01
tatyanakuchuk <tatyana8787@gmail.com>
2019-12-21 23:48:55 +0100

    Регистрация на сайте

--- signup.php
+++ signup.php
@@ -20,9 +20,110 @@ if(!$connect) {
         $error = mysqli_error($connect);
         print('Ошибка MySQL: ' . $error);
     }
+
+    // 2. send form
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $sign_up = $_POST;
+
+        $required = ['email', 'password', 'name'];
+        $errors = [];
+        $dict = [
+            'email' => 'E-mail',
+            'password' => 'Пароль',
+            'name' => 'Имя',
+            'avatar' => 'Фото'
+        ];
+
+        foreach($required as $key) {
+            if (empty($_POST[$key])) {
+                $errors[$key] = 'Это поле должно быть заполнено';
+            }
+        }
+
+        $email = $sign_up['email'];
+
+        //проверка email на корректность
+        if (!empty($email)) {
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors['email'] = 'Email должен быть корректным';
+            }
+        }
+
+        // проверка на существование пользователя с таким же email
+        if (!empty($email)) {
+            $sql = 'SELECT id FROM users WHERE email = "' . $email . '"';
+            $res_email = mysqli_query($connect, $sql);
+            if($res_email) {
+                $emails = mysqli_fetch_all($res_email, MYSQLI_ASSOC);
+                if(!empty($emails)) {
+                    $errors['email'] = 'Введённый вами email уже зарегистрирован. Введите другой email.';
+                }
+            }
+        }
+
+        // load avatar
+        if (isset($_FILES['avatar']['name'])) {
+            if(!empty($_FILES['avatar']['name'])) {
+                $tmp_name = $_FILES['avatar']['tmp_name'];
+                $file = $_FILES['avatar']['name'];
+
+                $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                $file_type = finfo_file($finfo, $tmp_name);
+
+                // Получаем расширение загруженного файла
+                $extension = strtolower(substr(strrchr($file, '.'), 1));
+                //Генерируем новое имя файла
+                $file = uniqid() . '.' .  $extension;
+                //Папка назначения
+                $dest = 'uploads/avatar/';
+
+                if (($file_type == "image/gif") || ($file_type == "image/jpeg") || ($file_type == "image/png") || ($file_type == "image/pjepg")) {
+                    move_uploaded_file($tmp_name, $dest . $file);
+                    $sign_up['avatar_path'] = $dest . $file;
+                }
+                else {
+                    $errors['avatar'] = 'Файл с таким расширением невозможно загрузить';
+                }
+            }
+        }
+
+        if (count($errors)) {
+            $signup_form = include_template('signup-form.php', [
+                'sign_up' => $sign_up,
+                'errors' => $errors,
+                'dict' => $dict
+            ]);
+        }
+        else {
+            // хэш от пароля
+            $password = password_hash($sign_up['password'], PASSWORD_DEFAULT);
+
+            $sql = 'INSERT INTO users (dt_add, name, email, password, avatar_path) ' .
+                    'VALUES (NOW(), ?, ?, ?, ?)';
+
+            $stmt = db_get_prepare_stmt($connect, $sql, [
+                $sign_up['name'],
+                $sign_up['email'],
+                $password,
+                $sign_up['avatar_path']
+            ]);
+
+            $res = mysqli_stmt_execute($stmt);
+
+            if ($res) {
+                $user_id = mysqli_insert_id($connect);
+                header('Location: /signin.php');
+            }
+
+        }
+
+    }
+    else {
+        $signup_form = include_template('signup-form.php');
+    }
 }
 
-$signup_form = include_template('signup-form.php');
+// $signup_form = include_template('signup-form.php');
 
 $page_content = include_template('main.php', [
     'form' => $signup_form,
--- templates/signup-form.php
+++ templates/signup-form.php
@@ -1,31 +1,59 @@
-<form class="form" action="" method="post" enctype="multipart/form-data">
+<form class="form" action="../signup.php" method="post" enctype="multipart/form-data">
     <div class="form__column">
         <!-- Сообщение об ошибках -->
-        <div class="form__errors">
-            <p>Пожалуйста, исправьте следующие ошибки:</p>
-            <ul>
-                <li><strong>E-mail:</strong> Это поле должно быть заполнено</li>
-                <li><strong>Пароль:</strong> Это поле должно быть заполнено</li>
-                <li><strong>Имя:</strong> Это поле должно быть заполнено</li>
-            </ul>
-        </div>
+        <?php if(isset($errors)) : ?>
+            <div class="form__errors">
+                <p>Пожалуйста, исправьте следующие ошибки:</p>
+                <ul>
+                    <?php foreach($errors as $error => $val) : ?>
+                        <li><strong><?= $dict[$error]; ?>:</strong> <?= $val; ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endif; ?>
         <!-- end Сообщение об ошибках -->
+
+        <?php $classname = isset($errors['email']) ? "form__input--error" : "";
+        $value = isset($sign_up['email']) ? $sign_up['email'] : ""; ?>
         <div class="form__row">
             <label class="form__label" for="email">E-mail:</label>
-            <input class="form__input " type="text" name="signup[email]" id="email" value="" placeholder="Ваш e-mail">
+            <input class="form__input <?= $classname; ?>" type="text" name="email" id="email" value="<?= $value; ?>" placeholder="Ваш e-mail">
+            <?php if(isset($errors['email'])) : ?>
+                <div class="error-notice">
+                    <span class="error-notice__icon"></span>
+                    <span class="error-notice__tooltip">Это поле должно быть заполнено</span>
+                </div>
+            <?php endif; ?>
         </div>
+        <?php $classname = isset($errors['password']) ? "form__input--error" : "";
+        $value = isset($sign_up['password']) ? $sign_up['password'] : ""; ?>
         <div class="form__row">
             <label class="form__label" for="password">Пароль:</label>
-            <input class="form__input " type="password" name="signup[password]" id="password" value="" placeholder="Задайте пароль">
+            <input class="form__input <?= $classname; ?>" type="password" name="password" id="password" value="<?= $value; ?>" placeholder="Задайте пароль">
+            <?php if(isset($errors['password'])) : ?>
+                <div class="error-notice">
+                    <span class="error-notice__icon"></span>
+                    <span class="error-notice__tooltip">Это поле должно быть заполнено</span>
+                </div>
+            <?php endif; ?>
         </div>
+        <?php $classname = isset($errors['name']) ? "form__input--error" : "";
+        $value = isset($sign_up['name']) ? $sign_up['name'] : ""; ?>
         <div class="form__row">
             <label class="form__label" for="nickname">Имя:</label>
-            <input class="form__input " type="text" name="signup[name]" id="nickname" value="" placeholder="Ваш никнейм на сайте">
+            <input class="form__input <?= $classname; ?>" type="text" name="name" id="nickname" value="<?= $value; ?>" placeholder="Ваш никнейм на сайте">
+            <?php if(isset($errors['name'])) : ?>
+                <div class="error-notice">
+                    <span class="error-notice__icon"></span>
+                    <span class="error-notice__tooltip">Это поле должно быть заполнено</span>
+                </div>
+            <?php endif; ?>
         </div>
+        <?php $value = isset($sign_up['avatar_path']) ? $sign_up['avatar_path'] : "Выбрать файл:"; ?>
         <div class="form__row">
             <label class="form__label" for="avatar">Фото:</label>
             <div class="form__input-file">
-                <input class="visually-hidden" type="file" name="signup[avatar]" id="preview" value="Выбрать файл:">
+                <input class="visually-hidden" type="file" name="avatar" id="preview" value="<?= $value; ?>">
                 <label for="preview">
                     <span>Выбрать файл</span>
                 </label>
